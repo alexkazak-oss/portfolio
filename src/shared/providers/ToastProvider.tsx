a7968ca359be9780d487d2eec80f28fe2d79f5cb
alexandrkazak-oss <alexandr.kazak.v@gmail.com>
2025-04-17 19:47:52 +0200

    Fix

--- src/shared/providers/ToastProvider.tsx
+++ src/shared/providers/ToastProvider.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { Toaster } from '@/components/ui'
+import { Toaster } from '@/components/ui/Toaster'
 
 export function ToastProvider() {
 	return <Toaster position='bottom-right' duration={8000} />
