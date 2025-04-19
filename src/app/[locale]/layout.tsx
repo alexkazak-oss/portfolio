6d9bd282ee7f0bd91092a8b3fb096a509f2827df
alexandrkazak-oss <alexandr.kazak.v@gmail.com>
2025-04-20 01:29:19 +0200

    fix

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,35 +1,33 @@
 import { notFound } from 'next/navigation'
 import { MainProvider } from '../providers'
 import { NextIntlClientProvider } from 'next-intl'
-
+import ClientLayout from './client-layout'
 export function generateStaticParams() {
   return [{ locale: 'en' }, { locale: 'it' }]
 }
 
 export const dynamicParams = false
 
-type Props = {
-  children: React.ReactNode
-  params: { locale: string }
-}
-
-export default async function LocaleLayout({ children, params }: Props) {
-  const { locale } = params
-
-  let messages
-  try {
-    messages = (await import(`../../messages/${locale}.json`)).default
-  } catch {
-    notFound()
-  }
-
-  return (
-        <NextIntlClientProvider locale={locale} messages={messages}>
-          <main className='flex flex-col min-h-screen w-full'>
-            <MainProvider locale={locale} messages={messages}>
-              {children}
-            </MainProvider>
-          </main>
-        </NextIntlClientProvider>
-  )
-}
+export default async function LocaleLayout(props: Promise<{
+	children: React.ReactNode
+	params: { locale: string }
+ }>) {
+	const { children, params } = await props
+	const { locale } = params
+ 
+	let messages
+	try {
+	  messages = (await import(`../../messages/${locale}.json`)).default
+	} catch {
+	  notFound()
+	}
+ 
+	return (
+			<NextIntlClientProvider locale={locale} messages={messages}>
+			  <MainProvider locale={locale} messages={messages}>
+				 <ClientLayout>{children}</ClientLayout>
+			  </MainProvider>
+			</NextIntlClientProvider>
+	)
+ }
+ 
